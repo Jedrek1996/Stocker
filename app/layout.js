39af0c6ba5343220f428fac11a1cf7bd4fe94d64
Jed <kohjedrek@yahoo.com.sg>
2024-07-29 00:45:01 +0800

    Updated providers

--- app/layout.js
+++ app/layout.js
@@ -12,7 +12,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider afterSignOutUrl="/">
+    <ClerkProvider
+      afterSignOutUrl="/"
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+    >
       <html lang="en">
         <body className={inter.className}>
           <Providers>{children}</Providers>
--- app/providers.jsx
+++ app/providers.jsx
@@ -1,11 +1,23 @@
+"use client";
 import { Toaster } from "react-hot-toast";
+import { useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const Providers = ({ children }) => {
+  const [queryClient] = useState(() => {
+    return new QueryClient({
+      queries: {
+        staleTime: 60 * 1000,
+      },
+    });
+  });
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Toaster position="top-center" />
       {children}
-    </>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
 

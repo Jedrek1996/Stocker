d9f5d1874c9c127daca221a352637a2297afe775
Jed <kohjedrek@yahoo.com.sg>
2024-07-29 00:47:40 +0800

    Added react query to stpckerai and queries route

--- app/(dashboard)/new-stock/page.js
+++ app/(dashboard)/new-stock/page.js
@@ -3,14 +3,13 @@ import {
   HydrationBoundary,
   QueryClient,
 } from "@tanstack/react-query";
-import Chat from "@/components/chat/Chat";
+import NewStock from "@/components/stock/new-stock";
 
 const page = () => {
   const queryClient = new QueryClient();
   return (
-    //hydration in this context is to "hydrate" or populate the frontend with data that was pre-fetched and prepared on the backend.
     <HydrationBoundary state={dehydrate(queryClient)}>
-      <Chat />
+      <NewStock />
     </HydrationBoundary>
   );
 };
